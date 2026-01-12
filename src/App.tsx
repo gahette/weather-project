1d6bd196acab6a64840f46302aceeed19db8dd7c
gahette <gahette@icloud.com>
2026-01-12 19:04:00 +0100

    Side Panel

--- src/App.tsx
+++ src/App.tsx
@@ -14,73 +14,77 @@ import CurrentSkeleton from "./components/skeletons/CurrentSkeleton";
 import DailySkeleton from "./components/skeletons/DailySkeleton";
 import HourlySkeleton from "./components/skeletons/HourlySkeleton";
 import AdditionalInfoSkeleton from "./components/skeletons/AdditionalInfoSkeleton";
+import SidePanel from "./components/SidePanel";
 
 function App() {
-    const [coordinates, setCoords] = useState<Coords>({
-        lat: 48.1436,
-        lon: 0.1622,
-    });
-    const [location, setLocation] = useState("Paris");
-    const [mapType, setMapType] = useState("clouds_new");
+	const [coordinates, setCoords] = useState<Coords>({
+		lat: 48.1436,
+		lon: 0.1622,
+	});
+	const [location, setLocation] = useState("Paris");
+	const [mapType, setMapType] = useState("clouds_new");
 
-    const { data: geocodeData } = useQuery({
-        queryKey: ["geocode", location],
-        queryFn: () => getGeocode(location),
-    });
+	const { data: geocodeData } = useQuery({
+		queryKey: ["geocode", location],
+		queryFn: () => getGeocode(location),
+	});
 
-    const onMapClick = (lat: number, lon: number) => {
-        setCoords({ lat, lon });
-        setLocation("custom");
-    };
+	const onMapClick = (lat: number, lon: number) => {
+		setCoords({ lat, lon });
+		setLocation("custom");
+	};
 
-    const coords =
-        location === "custom"
-            ? coordinates
-            : {
-                  lat: geocodeData?.[0].lat ?? coordinates.lat,
-                  lon: geocodeData?.[0].lon ?? coordinates.lon,
-              };
+	const coords =
+		location === "custom"
+			? coordinates
+			: {
+				lat: geocodeData?.[0].lat ?? coordinates.lat,
+				lon: geocodeData?.[0].lon ?? coordinates.lon,
+			};
 
-    return (
-        <div className="flex flex-col gap-8">
-            <div className="flex gap-8">
-                <div className="flex gap-4">
-                    <h1 className="text-2xl font-semibold">Location: </h1>
-                    <LocationDropdown
-                        location={location}
-                        setLocation={setLocation}
-                    />
-                </div>
-                <div className="flex gap-4">
-                    <h1 className="text-2xl font-semibold">Map Type: </h1>
-                    <MapTypeDropdown
-                        mapType={mapType}
-                        setMapType={setMapType}
-                    />
-                </div>
-            </div>
-            <div className="relative">
-                <Map
-                    coords={coords}
-                    onMapClick={onMapClick}
-                    mapType={mapType}
-                />
-                <MapLegend mapType={mapType} />
-            </div>
-            <Suspense fallback={<CurrentSkeleton />}>
-                <CurrentWeather coords={coords} />
-            </Suspense>
-            <Suspense fallback={<HourlySkeleton />}>
-                <HourlyForecast coords={coords} />
-            </Suspense>
-            <Suspense fallback={<DailySkeleton />}>
-                <DailyForecast coords={coords} />
-            </Suspense>
-            <Suspense fallback={<AdditionalInfoSkeleton />}>
-                <AdditionalInfo coords={coords} />
-            </Suspense>
-        </div>
-    );
+	return (
+		<>
+			<div className="flex flex-col gap-8">
+				<div className="flex gap-8">
+					<div className="flex gap-4">
+						<h1 className="text-2xl font-semibold">Location: </h1>
+						<LocationDropdown
+							location={location}
+							setLocation={setLocation}
+						/>
+					</div>
+					<div className="flex gap-4">
+						<h1 className="text-2xl font-semibold">Map Type: </h1>
+						<MapTypeDropdown
+							mapType={mapType}
+							setMapType={setMapType}
+						/>
+					</div>
+				</div>
+				<div className="relative">
+					<Map
+						coords={coords}
+						onMapClick={onMapClick}
+						mapType={mapType}
+					/>
+					<MapLegend mapType={mapType} />
+				</div>
+				<Suspense fallback={<CurrentSkeleton />}>
+					<CurrentWeather coords={coords} />
+				</Suspense>
+				<Suspense fallback={<HourlySkeleton />}>
+					<HourlyForecast coords={coords} />
+				</Suspense>
+				<Suspense fallback={<DailySkeleton />}>
+					<DailyForecast coords={coords} />
+				</Suspense>
+				<Suspense fallback={<AdditionalInfoSkeleton />}>
+					<AdditionalInfo coords={coords} />
+				</Suspense>
+			</div>
+			<SidePanel coords={coords} />
+		</>
+	);
 }
 
 export default App;
